f98a8a42c13c66c6e5a9c1df943595ee6c8d87a8
William Chuang <hsiangfu7@gmail.com>
2023-05-17 17:49:04 -0400

    done

---
+++ HsiangWeatherSizzler/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  HsiangWeatherSizzler
+//
+//  Created by Hsiang Chuang on 5/16/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+}
+
